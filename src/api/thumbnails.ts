99cd2bf3706a296ab9dff908a6505cca2ba7fac1
iahta <153889016+iahta@users.noreply.github.com>
2025-10-27 16:07:22 -0500

    upload thumbnail

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -11,6 +11,7 @@ type Thumbnail = {
 };
 
 const videoThumbnails: Map<string, Thumbnail> = new Map();
+const MAX_UPLOAD_SIZE = (10 << 20)
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -47,7 +48,31 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const parsed = await req.formData()
+  const img_data = parsed.get("thumbnail")
+  if (!(img_data !instanceof File)) {
+    throw new BadRequestError("Invalid file")
+  }
+  if (img_data.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File size must be below 10mb")
+  }
+  const media_type = img_data.type
+  let buf = await img_data.arrayBuffer()
+  const video_meta = getVideo(cfg.db, videoId)
+  if (!video_meta) {
+    throw new BadRequestError("Unable to locate video meta data")
+  }
+  if (video_meta.userID !== userID) {
+    throw new UserForbiddenError("Video does not belong to user")
+  }
+  videoThumbnails.set(video_meta.id, {
+    data: buf,
+    mediaType: media_type,
+  })
+  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`
+  video_meta.thumbnailURL = thumbnailURL
+  updateVideo(cfg.db, video_meta)
+  console.log(video_meta.thumbnailURL)
 
-  return respondWithJSON(200, null);
+  return respondWithJSON(200, video_meta);
 }
